8e10662f42199432e9d1c8ca12d82b1ddb65bc85
imrany <imranmat254@gmail.com>
2024-07-25 16:49:30 +0300

    create a resized folder and stores the resized images in there

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 use image::imageops::FilterType;
 use image::DynamicImage;
-use std::path::Path;
+use std::{
+    fs,
+    path::Path
+};
+
 fn main() {
     // Load the image from a file
-    let image_path=Path::from("./icon-256x256.png");
+    let image_path=Path::new("./icon-256x256.png");
     let img = image::open(&image_path);
     match img{
         Ok(v)=>{
-            println!("Image opened {}",image_path.extension());
+            println!("Image opened",);
             // Resize dimensions
             let new_width = 800;
             let new_height = 700;
@@ -15,12 +19,18 @@ fn main() {
             // Resize the image
             let resized_img = resize_image(&v, new_width, new_height);
 
-            // Save the resized image to a file
-            match resized_img.save(format!("./resized_icon-{new_width}x{new_height}.png")){
+            let create_resized_image_folder=fs::create_dir("./resized");
+            match create_resized_image_folder{
                 Ok(_)=>{
-                    println!("Image resized")
+                    // Save the resized image to a file
+                    match resized_img.save(format!("./resized/{}-{new_width}x{new_height}.{}",image_path.file_stem().unwrap().to_str().unwrap(),image_path.extension().unwrap().to_str().unwrap())){
+                        Ok(_)=>{
+                            println!("Image resized")
+                        },
+                        Err(e)=>println!("Failed to save resized image: {e}")
+                    }
                 },
-                Err(e)=>println!("Failed to save resized image: {e}")
+                Err(_)=>eprintln!("Cannot create a folder for the resized images")
             }
         },
         Err(e)=>println!("Failed to open image: {e}")
