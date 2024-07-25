66f43a5283336be201c9e75d4b8961c9556f3efa
imrany <imranmat254@gmail.com>
2024-07-25 18:48:53 +0300

    pivote to only resizing images

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,26 @@ use std::{
     fs,
     path::Path
 };
+use clap::Parser;
+
+/// A image resizing CLI tool
+#[derive(Parser, Debug)]
+#[command(author="Imrany <imranmat254@gmail.com>", version, about="A image resizing CLI tool.", long_about = None)]
+struct Args {
+    /// Source image path
+    #[arg(short, long)]
+    input: Path,
+
+    /// Output folder path
+    #[arg(short, long)]
+    output: Path,
+
+}
 
 fn main() {
-    // Load the image from a file
-    let image_path=Path::new("./icon-256x256.png");
-    let img = image::open(&image_path);
+    let args = Args::parse();
+
+    let img = image::open(&args.input);
     match img{
         Ok(v)=>{
             println!("Image opened",);
@@ -19,11 +34,11 @@ fn main() {
             // Resize the image
             let resized_img = resize_image(&v, new_width, new_height);
 
-            let create_resized_image_folder=fs::create_dir("./resized");
+            let create_resized_image_folder=fs::create_dir(&arg.output);
             match create_resized_image_folder{
                 Ok(_)=>{
                     // Save the resized image to a file
-                    match resized_img.save(format!("./resized/{}-{new_width}x{new_height}.{}",image_path.file_stem().unwrap().to_str().unwrap(),image_path.extension().unwrap().to_str().unwrap())){
+                    match resized_img.save(format!("./{}/{}-{new_width}x{new_height}.{}",&args.output,image_path.file_stem().unwrap().to_str().unwrap(),image_path.extension().unwrap().to_str().unwrap())){
                         Ok(_)=>{
                             println!("Image resized")
                         },
