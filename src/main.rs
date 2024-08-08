f01b7075a17bb4df8ab62cfe4f28a953647005e2
imrany <imranmat254@gmail.com>
2024-08-08 18:13:39 +0300

    added extension option

--- src/main.rs
+++ src/main.rs
@@ -18,19 +18,30 @@ struct Args {
     #[arg(short, long)]
     output: PathBuf,
 
+    /// output extension e.g -e=ico
+    #[arg(short, long)]
+    extension: Option<String>,
+
+    /// Enter resizing values, width and length i.e -v=[width,length]
+    #[arg(short, long)]
+    values: Option<Vec<u32>>
 }
 
 fn main() {
     let args = Args::parse();
 
+
+    let ext=match args.extension {
+        Some(ext)=>ext,
+        None=>format!("{}",args.input.extension().unwrap().to_str().unwrap())
+    };
+
     let img = image::open(&args.input);
     match img{
         Ok(v)=>{
-            // Resize dimensions
-            let square_dimensions:Vec<u32>=vec![16,32,48,72,96,128,144,152,192,384,512,57,60,76,114,120,180];
-            for i in 0..square_dimensions.len(){
-                let new_width=square_dimensions[i];
-                let new_height=square_dimensions[i];
+            if let Some(value)=&args.values{
+                let new_width=value[0];
+                let new_height=value[1];
                 // Resize the image
                 let resized_img = resize_image(&v, new_width, new_height);
 
@@ -38,9 +49,9 @@ fn main() {
                 match create_resized_image_folder{
                     Ok(_)=>{
                         // Save the resized image to a file
-                        match resized_img.save(format!("./{}/{}-{new_width}x{new_height}.{}",&args.output.to_str().unwrap(),args.input.file_stem().unwrap().to_str().unwrap(),args.input.extension().unwrap().to_str().unwrap())){
+                        match resized_img.save(format!("./{}/{}-{new_width}x{new_height}.{}",&args.output.to_str().unwrap(),args.input.file_stem().unwrap().to_str().unwrap(),&ext)){
                             Ok(_)=>{
-                                println!("./{}/{}-{new_width}x{new_height}.{}",&args.output.to_str().unwrap(),args.input.file_stem().unwrap().to_str().unwrap(),args.input.extension().unwrap().to_str().unwrap())
+                                println!("./{}/{}-{new_width}x{new_height}.{}",&args.output.to_str().unwrap(),args.input.file_stem().unwrap().to_str().unwrap(),&ext)
                             },
                             Err(e)=>eprintln!("Failed to save resized image: {e}")
                         }
@@ -48,11 +59,43 @@ fn main() {
                     Err(_)=>{
                         //eprintln!("{}",e);
                         // Save the resized image to a file
-                        match resized_img.save(format!("./{}/{}-{new_width}x{new_height}.{}",&args.output.to_str().unwrap(),args.input.file_stem().unwrap().to_str().unwrap(),args.input.extension().unwrap().to_str().unwrap())){
+                        match resized_img.save(format!("./{}/{}-{new_width}x{new_height}.{}",&args.output.to_str().unwrap(),args.input.file_stem().unwrap().to_str().unwrap(),&ext)){
                             Ok(_)=>{
-                                println!("./{}/{}-{new_width}x{new_height}.{}",&args.output.to_str().unwrap(),args.input.file_stem().unwrap().to_str().unwrap(),args.input.extension().unwrap().to_str().unwrap())
+                                println!("./{}/{}-{new_width}x{new_height}.{}",&args.output.to_str().unwrap(),args.input.file_stem().unwrap().to_str().unwrap(),&ext)
                             },
-                            Err(e)=>println!("Failed to save resized image: {e}")
+                            Err(e)=>eprintln!("Failed to save resized image: {e}")
+                        }
+                    }
+                }
+            }else{
+                // Resize dimensions
+                let square_dimensions:Vec<u32>=vec![16,32,48,72,96,128,144,152,192,384,512,57,60,76,114,120,180];
+                for i in 0..square_dimensions.len(){
+                    let new_width=square_dimensions[i];
+                    let new_height=square_dimensions[i];
+                    // Resize the image
+                    let resized_img = resize_image(&v, new_width, new_height);
+
+                    let create_resized_image_folder=fs::create_dir(&args.output);
+                    match create_resized_image_folder{
+                        Ok(_)=>{
+                            // Save the resized image to a file
+                            match resized_img.save(format!("./{}/{}-{new_width}x{new_height}.{}",&args.output.to_str().unwrap(),args.input.file_stem().unwrap().to_str().unwrap(),&ext)){
+                                Ok(_)=>{
+                                    println!("./{}/{}-{new_width}x{new_height}.{}",&args.output.to_str().unwrap(),args.input.file_stem().unwrap().to_str().unwrap(),&ext)
+                                },
+                                Err(e)=>eprintln!("Failed to save resized image: {e}")
+                            }
+                        },
+                        Err(_)=>{
+                            //eprintln!("{}",e);
+                            // Save the resized image to a file
+                            match resized_img.save(format!("./{}/{}-{new_width}x{new_height}.{}",&args.output.to_str().unwrap(),args.input.file_stem().unwrap().to_str().unwrap(),&ext)){
+                                Ok(_)=>{
+                                    println!("./{}/{}-{new_width}x{new_height}.{}",&args.output.to_str().unwrap(),args.input.file_stem().unwrap().to_str().unwrap(),&ext)
+                                },
+                                Err(e)=>println!("Failed to save resized image: {e}")
+                            }
                         }
                     }
                 }
